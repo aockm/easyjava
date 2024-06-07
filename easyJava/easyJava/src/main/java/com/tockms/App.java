c6df4c56a1153ebc8ffbe435a4d0ff920fd72609
tockm <3504747913@qq.com>
2024-06-07 17:48:52 +0800

    first commit

---
+++ easyJava/easyJava/src/main/java/com/tockms/App.java
@@ -0,0 +1,13 @@
+package com.tockms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
